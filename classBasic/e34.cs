bd66d0b18b34eb642864d9e7245ce2f23774476e
yes25 <programmer.yes25@gmail.com>
2023-05-15 14:50:55 +0900

    초기 커밋.

---
+++ classBasic/e34.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Math {
+        public static int abs(int i) {
+            return 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.abs(-1);
+            OtherClass oc = new OtherClass();
+
+            Console.WriteLine("Hello World!");
+
+            List<Student> list = new List<Student>() {
+                new Student() { name = "최승철", grade = 1 },
+                new Student() { name = "권순영", grade = 2 },
+                new Student() { name = "전원우", grade = 3 },
+                new Student() { name = "문준휘", grade = 4 },
+                new Student() { name = "유정한", grade = 1 },
+                new Student() { name = "홍지수", grade = 2 }
+                };
+            foreach (var item in list) { 
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+            for(int i = list.Count-1; i >= 0; i-- )
+            {
+                if (list[i].grade > 1)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        class ThridClass{ 
+        }
+    }
+}
---
+++ classBasic/program2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classBasic
+{
+    internal class program2
+    {
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
